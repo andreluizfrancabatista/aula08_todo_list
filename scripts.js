479f523a6d426be33c3d631149c12c761fcffa0d
andreluizfrancabatista <70104036+andreluizfrancabatista@users.noreply.github.com>
2025-05-06 15:09:26 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,36 @@
+function addTask(){
+    const input = document.getElementById("taskInput");
+    const taskText = input.value.trim();
+
+    if (taskText == ""){
+        alert("Digite uma tarefa!");
+        return;
+    }
+
+    const list = document.getElementById("todoList");
+    const li = document.createElement("li");
+    li.className = "todo-item";
+    li.innerHTML = `
+    <span class="taskText">
+    ${taskText}
+    </span>
+    <span>
+    <button class="done-btn" onclick="doneTask(this)">Feito!</button>
+    <button class="delete-btn" onclick="deleteTask(this)">Excluir</button>
+    </span>
+    `;
+
+    list.appendChild(li);
+    input.value = "";
+}
+
+function deleteTask(button){
+    const li = button.parentElement.parentElement;
+    li.remove();
+}
+
+function doneTask(button){
+    const texto = button.parentElement.previousElementSibling;
+    texto.classList.toggle("done");
+    button.classList.toggle("done-btn-a");
+}
